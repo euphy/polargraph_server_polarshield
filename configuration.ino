bfc87ee384fa191e53b4e38a83dafb84e4a53f01
Sandy Noble <sandy.noble@gmail.com>
2018-04-02 00:04:05 +0100

    SD card loading working again. Man that's an evening's work that I wish I'd done two years ago.

--- configuration.ino
+++ configuration.ino
@@ -99,7 +99,7 @@ void configuration_setup()
   stepsPerMM = multiplier(motorStepsPerRev) / mmPerRev;
 
   // init SD card
-  // sd_initSD();
+  sd_initSD();
   lcd_initLCD();
   lcd_showSummary();
   delay(1000);
